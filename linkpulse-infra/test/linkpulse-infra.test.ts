43857fc3e7222ab3e0372331445cf802408d9dad
berkoztrk <berkozturkw@outlook.com>
2025-10-25 00:42:02 +0300

    fix

---
+++ linkpulse-infra/test/linkpulse-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LinkpulseInfra from '../lib/linkpulse-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/linkpulse-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LinkpulseInfra.LinkpulseInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
